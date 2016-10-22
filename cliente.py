0afae5c420608a460da8d86e945e5b40c0ce5d87
AndresBarbosa <andresbob@ciencias.unam.mx>
2016-10-22 18:30:00 -0500

    Segundo backup

--- cliente.py
+++ cliente.py
@@ -18,7 +18,17 @@ class Cliente(QtGui.QMainWindow, clienteGui):
  		self.tableWidget.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff) #Tambien las verticales
  		self.ping.clicked.connect(self.lanzarPing)
  		self.participar.clicked.connect(self.partyHard)
- 		self.cliente = ServerProxy("http://127.0.0.1:8000") 
+ 		self.direccion = "http://" + self.url.text() + ":" + str(self.finder())
+ 		self.cliente = ServerProxy(self.direccion) 
+ 		self.tableWidget.keyPressEvent = self.keyPressEvent
+
+ 	def finder(self):
+ 		puerto = 0
+ 		if self.puerto.value() == 0:
+ 			puerto = 8000
+ 		else:
+ 			puerto = self.puerto.value()
+ 		return puerto
 
  	def lanzarPing(self):
  		self.ping.setText("Piging...")
@@ -32,6 +42,20 @@ class Cliente(QtGui.QMainWindow, clienteGui):
  		self.id.setText(str(snaker['id']))
  		self.color.setText(str(snaker['color']))
  		#self.participar.hide()
+
+ 	def keyPressEvent(self,event):
+ 		if event.key() == QtCore.Qt.Key_Left and self.dire != 1:
+ 			self.dire= 3
+ 		elif event.key() == QtCore.Qt.Key_Right and self.dire != 3:
+ 			self.dire= 1
+ 		elif event.key() == QtCore.Qt.Key_Up and self.dire != 2:
+ 			self.dire= 0
+ 		elif event.key() == QtCore.Qt.Key_Down and self.dire != 0:
+ 			self.dire= 2
+
+ 	def mueveDesdeCliente(self):
+ 		self.cliente.camba_direccion(self.id.text(), 2)
+
 
 
 #Inicia la aplicacion.
